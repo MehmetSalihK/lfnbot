83635a91e9d4863b368d8aaef0abf23a562244f7
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-05-03 12:26:00 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -33,19 +33,6 @@ var bot = new Discord.Client();
 
 var servers = {};
 
-bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "📈à-rejoin📈").send(`**Bienvenue à** __**${member.guild.name}**__ **maintenant** __**${member.user.username}**__ **est avec nous!**`)
-})
-
-bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "📉à-quitter📉").send(`__**${member.user.username}**__ **n'est plus sur notre serveur** __**${member.guild.name}**__`)
-})
-
-bot.on('guildMemberAdd', member => {
-    var role = member.guild.roles.find("name", "✓NDNG✓");
-    member.addRole(role)
-})
-
 bot.on("ready", async () => {
 
 	console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
