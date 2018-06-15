486b67c1e0d5758169bb24cc6792e390de429ec4
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-06-15 05:28:55 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
 const YTDL = require("ytdl-core");
 
 const TOKEN = "NDU2NTkyMDkwMDcyMDg4NTk4.DgMyIw.3BMj4oqfzRnNDaKSCbI-fDP-VXI";
@@ -47,82 +48,46 @@ bot.on("message", function(message) {
         message.delete (3000);
         message.reply("Welcome To Do LesFamilles NDNG. :)").then(d_msg => { d_msg.delete(30000); });
     }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
 
     if (message.content == "salut") {
         message.delete (3000);
         message.reply("Welcome To Do LesFamilles NDNG. :)").then(d_msg => { d_msg.delete(30000); });
     }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
 
     if (message.content == "Wesh") {
         message.delete (3000);
         message.reply("Welcome To Do LesFamilles NDNG. :)").then(d_msg => { d_msg.delete(30000); });
     }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
 
     if (message.content == "wesh") {
         message.delete (3000);
         message.reply("Welcome To Do LesFamilles NDNG. :)").then(d_msg => { d_msg.delete(30000); });
     }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
 
     if (message.content == "Wsh") {
         message.delete (3000);
         message.reply("Welcome To Do LesFamilles NDNG. :)").then(d_msg => { d_msg.delete(30000); });
     }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
 
     if (message.content == "wsh") {
         message.delete (3000);
         message.reply("Welcome To Do LesFamilles NDNG. :)").then(d_msg => { d_msg.delete(30000); });
     }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
 
     if (message.content == "CC") {
         message.delete (3000);
         message.reply("Welcome To Do LesFamilles NDNG. :)").then(d_msg => { d_msg.delete(30000); });
     }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
 
     if (message.content == "cc") {
         message.delete (3000);
         message.reply("Welcome To Do LesFamilles NDNG. :)").then(d_msg => { d_msg.delete(30000); });
     }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
 
     if (message.content == "welcome") {
         message.delete (3000);
         message.reply("Welcome To Do LesFamilles NDNG. :)").then(d_msg => { d_msg.delete(30000); });
     }
-});
-
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
 
     if (message.content == "Welcome") {
         message.delete (3000);
@@ -172,21 +137,59 @@ bot.on('ready', () => {
     });
   });
 
-bot.on("guildMemberAdd", function(member) {
-    member.guild.channels.find("name", "general").sendMessage(member.toString() + " WELCOME!");
+  bot.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', '📈à-rejoin📈');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | Participant dans notre serveur : ', `${member}`)
+        .addField(':microphone2: | Bienvenue!', `Bienvenue sur notre serveur, ${member}`)
+        .addField(':id: | Utilisateur :', "**[" + `${member.id}` + "]**")
+        .addField(':family_mwgb: | Vous êtes ', `${member.guild.memberCount}` + "ième personnes")
+        .addField("Nom", `<@` + `${member.id}` + `>`, true)
+        .addField('Serveur', `${member.guild.name}`, true )
+        .setFooter(` Pour la commande d'aide [-aide]`, "https://imgur.com/rAgsGxu.png")
+        .setTimestamp()
 
-    member.addRole(member.guild.roles.find("name", "〰️ KEJNE 〰️"));
-
-    member.guild.createRole({
-        name: member.user.username,
-        color: generateHex(),
-        permissions: []
-    }).then(function(role) {
-        member.addRole(role);
-    });
+        channel.sendEmbed(embed);
 });
 
-bot.on("message", function(message) {
+bot.on('guildMemberAdd', member => {
+
+    console.log(`${member}`, "relié" + `${member.guild.name}`)
+
+});
+
+bot.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', '📉à-quitter📉');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('Nom:', `${member}`)
+        .addField('Pourquoi ?', "qui t'a soutenu ?")
+        .addField('Bye Bye :(', 'Ouvre ton chemin, mon frère/ma soeur!')
+        .addField('Maintenant sur notre serveur ils nous reste', `${member.guild.memberCount}` + " personnes")
+        .setFooter(` Pour la commande d'aide [-aide]`, "https://imgur.com/rAgsGxu.png")
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberRemove', member => {
+    console.log(`${member}` + "ont été divisés" + `${member.guild.name}` + "Laissez un message maintenant")
+    console.log("Message envoyé")
+});
+
+bot.on('guildMemberAdd', member => {
+    var role = member.guild.roles.find("name", "✓NDNG✓");
+    member.addRole(role)
+})
+
+  bot.on("message", function(message) {
     if (message.author.equals(bot.user)) return;
 
     if (!message.content.startsWith(PREFIX)) return;
